9bf08d46266cc7a6ea58fd146a6a85336bd10cce
Allen Nguyen <anhn321@gmail.com>
2023-12-23 23:48:40 -0800

    Change name to Musicboxd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import HelloWorld from './HelloWorld';
 
 function App() {
 	return (
 		<>
 			<h1 className='text-3xl font-bold underline bg-slate-800'>
-				Spotigram
+				Musicboxd
 			</h1>
 		</>
 	);
