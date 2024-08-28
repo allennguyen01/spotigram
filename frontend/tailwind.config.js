ed4e81e1aca96c3442b90348528a615fde73bd18
Allen Nguyen <anhn321@gmail.com>
2024-08-28 00:22:50 -0700

    feat: Add Google Fonts and update font styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,8 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				playfair: ['Playfair Display'],
+				playfair: ['"Playfair Display"', 'serif'],
+				inter: ['Inter', 'sans-serif'],
 			},
 		},
 	},
