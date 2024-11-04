2166714ed18c10961caecd4a938cb75a446effa5
Allen Nguyen <anhn321@gmail.com>
2024-11-03 22:55:24 -0800

    feat: get 12 latest albums releases

---
+++ frontend/src/config/spotifyClient.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+
+async function getAccessToken() {
+	const headers = {
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		auth: {
+			username: SPOTIFY_CLIENT_ID,
+			password: SPOTIFY_CLIENT_SECRET,
+		},
+	};
+
+	const data = {
+		grant_type: 'client_credentials',
+	};
+
+	const response = await axios.post(
+		'https://accounts.spotify.com/api/token',
+		data,
+		headers,
+	);
+	return response.data.access_token;
+}
+
+async function createSpotifyClient() {
+	const accessToken = await getAccessToken();
+
+	const instance = axios.create({
+		baseURL: 'https://api.spotify.com/v1/',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
+
+	return instance;
+}
+
+const spotifyClient = await createSpotifyClient();
+export default spotifyClient;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import spotifyClient from '../config/spotifyClient';
 
 export default function Home() {
-	return <div>HOME</div>;
+	const [newAlbums, setNewAlbums] = useState([]);
+
+	useEffect(() => {
+		async function getAlbums() {
+			const response = await spotifyClient.get('/browse/new-releases?limit=12');
+			setNewAlbums(response.data.albums.items);
+		}
+		getAlbums();
+	}, []);
+
+	return (
+		<div>
+			{newAlbums.map((album) => (
+				<div key={album.id}>
+					<img
+						src={album.images[0].url}
+						alt={album.name}
+					/>
+					<p>{album.name}</p>
+					<p>{album.artists.map((artist) => artist.name).join(', ')}</p>
+					<p>{album.release_date}</p>
+				</div>
+			))}
+		</div>
+	);
 }
