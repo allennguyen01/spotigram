0073d6f16baf9ca773e8a4cd72a596230c28a187
Allen Nguyen <anhn321@gmail.com>
2024-12-12 23:19:51 -0800

    refactor: clean up SearchResults to use spotifyClient

--- frontend/src/pages/SearchResults.tsx
+++ frontend/src/pages/SearchResults.tsx
@@ -1,65 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from 'react';
 import AlbumCard from '../components/AlbumCard.jsx';
 import { useParams } from 'react-router-dom';
-
-const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const SPOTIFY_CLIENT_SECRET = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+import spotifyClient from '../config/spotifyClient';
 
 export default function SearchResults() {
-	const { searchInput } = useParams();
-
-	const [accessToken, setAccessToken] = useState('');
+	const { searchInput = '' } = useParams();
 	const [albumSearch, setAlbumSearch] = useState([]);
 
 	useEffect(() => {
-		const headers = {
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			auth: {
-				username: SPOTIFY_CLIENT_ID,
-				password: SPOTIFY_CLIENT_SECRET,
-			},
-		};
-
-		const data = {
-			grant_type: 'client_credentials',
-		};
-
-		axios
-			.post('https://accounts.spotify.com/api/token', data, headers)
-			.then((response) => {
-				setAccessToken(response.data.access_token);
-			})
-			.catch((error) => {
-				console.error(error);
-			});
-	}, []);
-
-	function search() {
-		console.log(`Searching ${searchInput}`);
-
-		axios
-			.get(`https://api.spotify.com/v1/search?type=album&q=${searchInput}`, {
-				headers: {
-					'Content-Type': 'application/json',
-					Authorization: `Bearer ${accessToken}`,
-				},
-			})
-			.then((response) => {
-				let albums = response.data.albums.items.filter(
-					(item) => item.album_type === 'album',
+		async function search() {
+			try {
+				const res = await spotifyClient.get(
+					`https://api.spotify.com/v1/search?type=album&q=${searchInput}`,
+				);
+				const albums = res.data.albums.items.filter(
+					(item: { album_type: string }) => item.album_type === 'album',
 				);
 				setAlbumSearch(albums);
-			})
-			.catch((error) => {
+			} catch (error) {
 				console.error(error);
-			});
-	}
+			}
+		}
 
-	useEffect(search, [searchInput, accessToken]);
+		search();
+	}, [searchInput]);
 
 	return (
 		<div className='flex flex-col items-center justify-center'>
