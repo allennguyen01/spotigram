7ddfb5a7486736d7e57aca66a17d4d667a505d49
Allen Nguyen <anhn321@gmail.com>
2024-12-16 08:42:08 -0800

    feat: style bg image in login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,14 +3,14 @@ import SignUpModal from '../components/SignUpModal';
 export default function Home() {
 	return (
 		<div
-			className='blurred-edges hero min-h-lvh max-w-screen-xl'
+			className='blurred-edges hero min-h-[calc(99lvh-4rem)] max-w-screen-xl'
 			style={{
 				backgroundImage:
 					'url(https://media.cnn.com/api/v1/images/stellar/prod/130907221429-jukebox-1942.jpg?q=w_3580,h_2340,x_0,y_0,c_fill)',
 			}}
 		>
 			<div className='hero-overlay bg-opacity-40'></div>
-			<div className='hero-content mb-10 h-full items-end text-center'>
+			<div className='hero-content mb-10 items-end text-center'>
 				<div className='flex flex-col items-center justify-center gap-6'>
 					<p className='mb-5 font-playfair text-5xl font-bold text-white'>
 						Track albums you&apos;ve listened to.
