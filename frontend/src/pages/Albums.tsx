5b06dbc07dc1d892def65817e6aa2bb01415cc87
Allen Nguyen <anhn321@gmail.com>
2024-12-18 10:42:29 -0800

    feat: add hiphop and R&B album carousels

--- frontend/src/pages/Albums.tsx
+++ frontend/src/pages/Albums.tsx
@@ -21,6 +21,16 @@ type NewAlbumCards = {
 
 export default function Home() {
 	const [newAlbums, setNewAlbums] = useState([]);
+	const [hiphopAlbums, setHipHopAlbums] = useState([]);
+	const [rbAlbums, setRBAlbums] = useState([]);
+
+	// TODO: add type checking
+	function filter20Albums(items: any) {
+		return items
+			.map((item: any) => item.album)
+			.filter((album: any) => album.album_type === 'album')
+			.slice(0, 20);
+	}
 
 	useEffect(() => {
 		async function getAlbums() {
@@ -30,42 +40,80 @@ export default function Home() {
 		getAlbums();
 	}, []);
 
+	// TODO: remove duplicate albums appearing, check ID
+	useEffect(() => {
+		async function getGenreAlbums() {
+			const response = await spotifyClient.get(
+				'search?q=genre%3A%22hip+hop%22+year%3A2024&type=track&market=US&limit=50',
+			);
+			setHipHopAlbums(filter20Albums(response.data.tracks.items));
+		}
+		getGenreAlbums();
+	}, []);
+
+	// TODO: remove duplicate albums appearing, check ID
+	useEffect(() => {
+		async function getGenreAlbums() {
+			const response = await spotifyClient.get(
+				'search?q=genre%3A%22r%26b%22+year%3A2024&type=track&market=US&limit=50',
+			);
+			setRBAlbums(filter20Albums(response.data.tracks.items));
+		}
+		getGenreAlbums();
+	}, []);
+
 	return (
 		<div className='p-4'>
 			<HeaderDivider
 				text='NEW ALBUM RELEASES'
 				className='mx-3 mb-2'
 			/>
-			<Carousel
-				className='w-full max-w-5xl'
-				opts={{ slidesToScroll: 4 }}
-			>
-				<CarouselContent>
-					{newAlbums.map((album: NewAlbumCards) => (
-						<CarouselItem
-							key={album.id}
-							className='flex basis-1/4 flex-col items-center gap-2 py-1 text-center'
-						>
-							<img
-								src={album.images[0].url}
-								alt={album.name}
-								className='box-border h-56 max-w-56 rounded transition duration-150 hover:cursor-pointer hover:shadow-white'
-							/>
-							<div className='flex max-w-56 flex-col'>
-								<span className='inline-flex items-center gap-1 font-semibold'>
-									{album.name}
-									<SpotifyIconButton spotifyURL={album.external_urls.spotify} />
-								</span>
-								<p className='text-sm'>
-									{album.artists.map((artist) => artist.name).join(', ')}
-								</p>
-							</div>
-						</CarouselItem>
-					))}
-				</CarouselContent>
-				<CarouselPrevious />
-				<CarouselNext />
-			</Carousel>
+			<FourAlbumCarousel newAlbums={newAlbums} />
+			<HeaderDivider
+				text='HIP HOP ALBUMS'
+				className='mx-3 mb-2'
+			/>
+			<FourAlbumCarousel newAlbums={hiphopAlbums} />
+			<HeaderDivider
+				text='R&B ALBUMS'
+				className='mx-3 mb-2'
+			/>
+			<FourAlbumCarousel newAlbums={rbAlbums} />
 		</div>
+	);
+}
+
+function FourAlbumCarousel({ newAlbums }: { newAlbums: NewAlbumCards[] }) {
+	return (
+		<Carousel
+			className='min-h-96 w-full max-w-5xl'
+			opts={{ slidesToScroll: 4 }}
+		>
+			<CarouselContent>
+				{newAlbums.map((album: NewAlbumCards) => (
+					<CarouselItem
+						key={album.id}
+						className='flex basis-1/4 flex-col items-center gap-2 py-1 text-center'
+					>
+						<img
+							src={album.images[0].url}
+							alt={album.name}
+							className='box-border h-56 max-w-56 rounded transition duration-150 hover:cursor-pointer hover:shadow-white'
+						/>
+						<div className='flex max-w-56 flex-col'>
+							<span className='inline-flex items-center gap-1 font-semibold'>
+								{album.name}
+								<SpotifyIconButton spotifyURL={album.external_urls.spotify} />
+							</span>
+							<p className='text-sm'>
+								{album.artists.map((artist) => artist.name).join(', ')}
+							</p>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
 	);
 }
